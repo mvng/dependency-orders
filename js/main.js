f7da0a7a8a58d2eed9d504307257ea113d012611
mattvn <matthew.nguyen@me.com>
2015-10-15 00:57:08 -0700

    Basic Package install and Test Cases implemented

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-var INSTALL = "['KittenService: CamelCaser', 'CamelCaser: ']";
+function UserException(message) {
+   this.message = message;
+   this.name = "UserException";
+}
 
 function parse(packages) {
 
@@ -10,3 +13,72 @@ function parse(packages) {
   packages = packages.split(",");
   return packages;
 }
+
+//singleton object of packages to install.
+var packages = new function() {
+  this.size = 0;
+  this.installList = {};
+  this.uniquePackages = [];
+
+  this.installPackages = function() {
+    var installList = [];
+    var size = this.size;
+    var stage = [];
+    var counter = 0;
+   while (this.uniquePackages.length > 0) {
+     var packageName = this.uniquePackages[counter];
+     var current = this.installList[packageName];
+
+     //installs the package if it has no dependency.
+      if (current.dependency === false && !current.visited && !current.installed) {
+        current.visited = true;
+        current.installed = true;
+        stage.push(packageName);
+        this.uniquePackages.splice(counter, 1);
+        continue;
+      } else {
+      //Start to explore until no dependency package or installed
+        var dependencyStage = [];
+        dependencyStage.push(current.name);
+        while(current.dependency != false && !current.installed && !current.visited){
+          current.visited = true;
+          current = this.installList[current.dependency];
+          dependencyStage.push(current.name);
+
+         }
+         //Check if Dependency Trail has any Cycles by comparing the last value with everything else.
+         if (dependencyStage.indexOf(current.name) != dependencyStage.length -1) {
+            throw new UserException("Cycle Detected");
+         }
+         this.installMerge(dependencyStage, stage);
+       }
+   }
+     return stage;
+  }
+  this.installMerge = function(dependencyStage, stage) {
+    while(dependencyStage.length > 0) {
+      var toMerge = dependencyStage.pop();
+      this.installList[toMerge].installed = true;
+      this.uniquePackages.splice(this.uniquePackages.indexOf(toMerge), 1);
+     stage.push(toMerge);
+    }
+
+  }
+  this.add = function(name, dependency) {
+    var node;
+    if (typeof dependency === 'undefined' || dependency === ' ') {
+      dependency = false;
+    }
+    node = { 'name': name, 'dependency': dependency,'installed': false, 'visited': false};
+    this.installList[name] = node;
+    this.size++;
+    this.uniquePackages.push(name);
+  }
+
+  this.getUniquePackages = function () {
+    return this.uniquePackages;
+  }
+  this.getSize = function () {
+    return this.size;
+  }
+} 
--- test/test.js
+++ test/test.js
@@ -1,6 +1,36 @@
 describe("Input Parsing Test", function() {
   it("Basic Splitting", function() {
-    expect(parse("['KittenService: CamelCaser', 'CamelCaser: ']")[0]).toBe('KittenService:CamelCaser');
-    expect(parse("['KittenService: CamelCaser', 'CamelCaser: ']")[1]).toBe('CamelCaser:');
+    var message = parse("['KittenService: CamelCaser', 'CamelCaser: ']");
+    
+    expect(message[0]).toBe('KittenService:CamelCaser');
+    expect(message[1]).toBe('CamelCaser:');
   });
 });
+
+describe("Basic Installs", function () {
+  it("Basic Case A", function() {
+      var expectedOutput = ['X', 'ayy', 'Donger', 'CamelCaser', 'KittenService', 'b', 'a'];
+      packages.add('X', ' ');
+      packages.add('KittenService','CamelCaser');
+      packages.add('a','b');
+      packages.add('CamelCaser','Donger');
+      packages.add('Donger', 'ayy');
+      packages.add('ayy', ' ');
+      packages.add('b', ' ');
+      var output = packages.installPackages();
+      expect(output).toEqual(expectedOutput);  
+    });
+});
+
+describe("Cycle Catching",  function() {
+  it("Basic Cycle Case A", function() {
+  packages.add('A', 'B');
+  packages.add('B', 'C');
+  packages.add('C', 'A');
+  //expect( function(){ parser.parse(raw); } ).toThrow(new Error("Parsing is not possible"));
+
+  expect( function(){ packages.installPackages(); }).toThrow(new UserException("Cycle Detected"));
+  });
+
+});
+
